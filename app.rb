eb755a45afc03e6c2fc9144363c35559393ed43e
luba <lubo4ka.boiko@gmail.com>
2021-04-28 18:13:18 +0300

    logs

--- app.rb
+++ app.rb
@@ -9,11 +9,14 @@ puts "[#{Time.now}] started"
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
+    pp message.text
+ 
     if message.text.start_with?('/start')
       email = Base64.decode64(message.text.split('/start').join.strip)
       bot.api.send_message(chat_id: message.chat.id, text: "Hi, #{message.from.first_name} (#{email})")
       data = { email: email, chat_id: message.chat.id }
 
+      pp email
       uri = URI("#{ENV['TOYSHARE_URL']}/telegram_webhook")
       res = Net::HTTP.post_form(uri, *data)
       puts res.body
